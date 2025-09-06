4bc33a78b82e16d5a435984e454c32e2eaa93633
osamikoyo <v.pylayev@mail.ru>
2025-09-06 08:43:13 +0500

    add chifer

---
+++ chifer/chifer.go
@@ -0,0 +1,66 @@
+package chifer
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+)
+
+func Base64Encode(text string) string {
+	return base64.StdEncoding.EncodeToString([]byte(text))
+}
+
+func Base64Decode(encoded string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func AESEncrypt(text string, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	msg := []byte(text)
+	padding := aes.BlockSize - len(msg)%aes.BlockSize
+	padText := append(msg, byte(padding)*padding)
+
+	iv := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, len(padText))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext, padText)
+
+	final := append(iv, ciphertext...)
+	return base64.StdEncoding.EncodeToString(final), nil
+}
+
+func AESDecrypt(encrypted string, key []byte) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encrypted)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	iv := data[:aes.BlockSize]
+	ciphertext := data[aes.BlockSize:]
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	plaintext := make([]byte, len(ciphertext))
+	mode.CryptBlocks(plaintext, ciphertext)
+
+	padding := int(plaintext[len(plaintext)-1])
+	return string(plaintext[:len(plaintext)-padding]), nil
+}
