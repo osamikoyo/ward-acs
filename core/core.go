a4e3d7b355f07017fc9d3c3a325946475a7b550f
osamikoyo <v.pylayev@mail.ru>
2025-09-07 08:22:34 +0500

    update core

--- core/core.go
+++ core/core.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/osamikoyo/ward/chifer"
 	"github.com/osamikoyo/ward/config"
 	"github.com/osamikoyo/ward/entity/data"
 	"github.com/osamikoyo/ward/entity/grand"
@@ -17,7 +18,11 @@ import (
 	"github.com/osamikoyo/ward/repository"
 )
 
-var ErrPermissionDenied = errors.New("permission denied")
+var (
+	ErrPermissionDenied = errors.New("permission denied")
+	ErrEncrypt          = errors.New("failed encrypt")
+	ErrDecrypt          = errors.New("failed decrypt")
+)
 
 const TokenLength = 40
 
@@ -26,15 +31,17 @@ type WardCore struct {
 	logger     *logger.Logger
 
 	timeout time.Duration
+	key     []byte
 	cfg     *config.Config
 }
 
-func NewWardCore(repository *repository.Repository, logger *logger.Logger, cfg *config.Config, timeout time.Duration) *WardCore {
+func NewWardCore(repository *repository.Repository, logger *logger.Logger, cfg *config.Config, key []byte, timeout time.Duration) *WardCore {
 	return &WardCore{
 		repository: repository,
 		logger:     logger,
 		timeout:    timeout,
 		cfg:        cfg,
+		key:        key,
 	}
 }
 
@@ -69,13 +76,43 @@ func (w *WardCore) RegisterUser(token string, grandUID uuid.UUID) (string, error
 		return "", fmt.Errorf("failed generate token: %v", err)
 	}
 
-	user := user.NewUser(token, grandUID)
+	usrToken, err := w.generateToken()
+	if err != nil {
+		return "", err
+	}
+
+	user := user.NewUser(usrToken, grandUID)
 
 	if err := w.repository.CreateUser(ctx, user); err != nil {
 		return "", err
 	}
 
 	return tokenOut, nil
+}
+
+func (w *WardCore) CreateData(token string, payload string, doEnc bool, grandUID uuid.UUID) error {
+	ctx, cancel := w.context()
+	defer cancel()
+
+	user, err := w.repository.GetUserByToken(ctx, token)
+	if err != nil {
+		return err
+	}
+
+	if user.Grand.Name != w.cfg.RouteDataRole {
+		return ErrPermissionDenied
+	}
+
+	if doEnc {
+		payload, err = chifer.AESEncrypt(payload, w.key)
+		if err != nil {
+			return ErrEncrypt
+		}
+	}
+
+	data := data.NewData(grandUID, payload, doEnc)
+
+	return w.repository.CreateData(ctx, data)
 }
 
 func (w *WardCore) ChangeUserGrand(token string, userUID uuid.UUID, grandUID uuid.UUID) error {
@@ -112,6 +149,15 @@ func (w *WardCore) GetData(token string, dataUID uuid.UUID) (*data.Data, error) 
 		return nil, err
 	}
 
+	if data.Encrypted {
+		payload, err := chifer.AESDecrypt(data.Payload, w.key)
+		if err != nil {
+			return nil, ErrDecrypt
+		}
+
+		data.Payload = payload
+	}
+
 	if user.Grand.Level < data.Grand.Level {
 		return nil, ErrPermissionDenied
 	}
@@ -139,6 +185,22 @@ func (w *WardCore) ChangeDataGrand(token string, dataUID, grandUID uuid.UUID) er
 	return nil
 }
 
+func (w *WardCore) DeleteData(token string, dataUID uuid.UUID) error {
+	ctx, cancel := w.context()
+	defer cancel()
+
+	user, err := w.repository.GetUserByToken(ctx, token)
+	if err != nil {
+		return err
+	}
+
+	if user.Grand.Name != w.cfg.RouteDataRole {
+		return ErrPermissionDenied
+	}
+
+	return w.repository.DeleteData(ctx, dataUID)
+}
+
 func (w *WardCore) CreateGrand(token, name string, level int) error {
 	ctx, cancel := w.context()
 	defer cancel()
@@ -155,4 +217,36 @@ func (w *WardCore) CreateGrand(token, name string, level int) error {
 	grand := grand.NewGrand(name, level)
 
 	return w.repository.CreateGrand(ctx, grand)
+}
+
+func (w *WardCore) DeleteGrand(token string, uid uuid.UUID) error {
+	ctx, cancel := w.context()
+	defer cancel()
+
+	user, err := w.repository.GetUserByToken(ctx, token)
+	if err != nil {
+		return err
+	}
+
+	if user.Grand.Name != w.cfg.RouteGrandRole {
+		return ErrPermissionDenied
+	}
+
+	return w.repository.DeleteGrand(ctx, uid)
+}
+
+func (w *WardCore) ChangeGrandLevel(token string, grandUID uuid.UUID, level int) error {
+	ctx, cancel := w.context()
+	defer cancel()
+
+	user, err := w.repository.GetUserByToken(ctx, token)
+	if err != nil {
+		return err
+	}
+
+	if user.Grand.Name != w.cfg.RouteGrandRole {
+		return ErrPermissionDenied
+	}
+
+	return w.repository.UpdateGrand(ctx, grandUID, "level", level)
 }
