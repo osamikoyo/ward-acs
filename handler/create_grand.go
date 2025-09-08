c56f5d13812174fb20531cc601d1d02e558001a1
osamikoyo <v.pylayev@mail.ru>
2025-09-08 06:41:14 +0500

    add create_grand handler

---
+++ handler/create_grand.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) CreateGrandHandler(c echo.Context) error {
+	token := c.Param("token")
+
+	grand := struct {
+		Level int    `json:"level"`
+		Name  string `json:"name"`
+	}{}
+
+	if err := c.Bind(&grand); err != nil {
+		return c.String(http.StatusBadRequest, "failed bind body to grand")
+	}
+
+	if err := h.core.CreateGrand(token, grand.Name, grand.Level); err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusCreated, "ok")
+}
