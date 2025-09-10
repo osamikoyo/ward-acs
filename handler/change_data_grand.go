5a724acb2072419ac0c4eb19c665e02f5a3f87ee
osamikoyo <v.pylayev@mail.ru>
2025-09-10 15:07:04 +0500

    add change data grand handler

---
+++ handler/change_data_grand.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) ChangeDataGrand(c echo.Context) error {
+	token := c.Param("token")
+
+	req := struct {
+		Data  string `json:"data"`
+		Grand string `json:"grand"`
+	}{}
+
+	if err := c.Bind(&req); err != nil {
+		return c.String(http.StatusBadRequest, "failed bind body to req")
+	}
+
+	dataUID, err := uuid.Parse(req.Data)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "failed parse data uid")
+	}
+
+	grandUID, err := uuid.Parse(req.Grand)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "failed parse grand uid")
+	}
+
+	if err = h.core.ChangeDataGrand(token, dataUID, grandUID); err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusOK, "ok")
+}
