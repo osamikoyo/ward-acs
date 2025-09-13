ca6caa7c3d2e77164d1f32ba4c9cbf985806afa4
osamikoyo <v.pylayev@mail.ru>
2025-09-13 06:35:41 +0500

    update casher

--- casher/casher.go
+++ casher/casher.go
@@ -1,7 +1,42 @@
 package casher
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"time"
+
+	"github.com/osamikoyo/ward/logger"
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
 
 type Casher struct {
 	client *redis.Client
+	logger *logger.Logger
+}
+
+var (
+	ExpTime = time.Hour
+)
+
+func NewCasher(client *redis.Client, logger *logger.Logger) *Casher {
+	return &Casher{
+		client: client,
+		logger: logger,
+	}
+}
+
+func (c *Casher) AddToCash(ctx context.Context, key string, value string) error {
+	_, err := c.client.Set(ctx, key, value, ExpTime).Result()
+	if err != nil {
+		c.logger.Error("failed add to cash",
+			zap.String("key", key),
+			zap.String("value", value),
+			zap.Error(err))
+
+		return err
+	}
+
+	c.logger.Info("value was added to cash successfully", zap.String("value", value))
+
+	return nil
 }
