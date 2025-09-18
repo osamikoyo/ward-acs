ffe953c3de866d905c2e01b2a8608a22444d5ea0
osamikoyo <v.pylayev@mail.ru>
2025-09-18 07:05:40 +0500

    add delete data handler

---
+++ handler/delete_data.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handler) DeleteDataHandler(c echo.Context) error {
+	token := c.Param("token")
+
+	uid := c.Param("uid")
+
+	uuid, err := uuid.Parse(uid)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "failed parse uid")
+	}
+
+	if err = h.core.DeleteData(token, uuid); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	return c.String(http.StatusOK, "ok")
+}
