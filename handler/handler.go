4e7c9f0a71f1ccb4fc414fb2b1402d466e81b984
osamikoyo <v.pylayev@mail.ru>
2025-09-22 06:56:28 +0500

    update handler

--- handler/handler.go
+++ handler/handler.go
@@ -4,6 +4,8 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/osamikoyo/ward/core"
+
+	tokenmw "github.com/osamikoyo/ward/middleware"
 )
 
 type Handler struct {
@@ -12,11 +14,20 @@ type Handler struct {
 
 func (h *Handler) RegisterRouters(e *echo.Echo) {
 	e.Use(middleware.Logger())
+	e.Use(tokenmw.TokenMiddleware)
 
 	user := e.Group("/user")
 	user.POST("/register", h.RegisterUserHandler)
+	user.DELETE("/delete/:uid", h.DeleteUserHandler)
 
 	grand := e.Group("/grand")
-	grand.POST("/create/:token", h.CreateGrandHandler)
-	grand.DELETE("/delete/:token/uid/:uid", h.DeleteGrandHandler)
+	grand.POST("/create", h.CreateGrandHandler)
+	grand.DELETE("/delete//uid/:uid", h.DeleteGrandHandler)
+	grand.PATCH("/change/level/uid/:uid", h.ChangeGrandLevelHandler)
+
+	data := e.Group("/data")
+	data.POST("/create", h.CreateDataHandler)
+	data.PATCH("/change/grand/:uid", h.ChangeDataGrand)
+	data.DELETE("/delete/:uid", h.DeleteDataHandler)
+	data.GET("/list", h.ListDataHandler)
 }
