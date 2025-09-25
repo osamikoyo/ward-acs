f3ea87965d7f9fec88732db37327333d12859dd2
osamikoyo <v.pylayev@mail.ru>
2025-09-25 06:17:53 +0500

    update change data grand handler

--- handler/change_data_grand.go
+++ handler/change_data_grand.go
@@ -8,7 +8,10 @@ import (
 )
 
 func (h *Handler) ChangeDataGrand(c echo.Context) error {
-	token := c.Param("token")
+	token, ok := c.Get("token").(string)
+	if !ok {
+		return c.String(http.StatusBadGateway, "invalid token")
+	}
 
 	req := struct {
 		Data  string `json:"data"`
